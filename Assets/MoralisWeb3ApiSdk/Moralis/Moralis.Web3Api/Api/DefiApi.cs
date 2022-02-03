e7163904a940440a9aa6747d655d36c092ed75ca
xactant <xactant@gmail.com>
2022-02-03 16:48:30 -0500

    correct Web3Api.DefiApi namespace

--- Assets/MoralisWeb3ApiSdk/Moralis/Moralis.Web3Api/Api/DefiApi.cs
+++ Assets/MoralisWeb3ApiSdk/Moralis/Moralis.Web3Api/Api/DefiApi.cs
@@ -37,7 +37,7 @@ using Moralis.Web3Api.Client;
 using Moralis.Web3Api.Interfaces;
 using Moralis.Web3Api.Models;
 
-namespace Moralis.SolanaApi.Api
+namespace Moralis.Web3Api.Api
 {
 	/// <summary>
 	/// Represents a collection of functions to interact with the API endpoints
