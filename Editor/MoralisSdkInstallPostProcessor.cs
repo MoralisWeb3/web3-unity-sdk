db79e019f389fcaf42d3f4d346c2344a62c38932
xactant <xactant@gmail.com>
2022-03-10 16:57:23 -0500

    Move asset files to asset folder

--- Editor/MoralisSdkInstallPostProcessor.cs
+++ Editor/MoralisSdkInstallPostProcessor.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 using UnityEditor.PackageManager;
  
 using System.IO;
+using System.Linq;
 using System;
 using System.Collections.Generic;
 
@@ -33,9 +34,7 @@ namespace MoralisWeb3ApiSdk.Editor
 		{
             Debug.Log("Starting to Moralis SDK pre-process ...");
 
-            CopyWebGLTemplate();
-
-            UpdateCscFile();
+            PlayerSettings.WebGL.template = "MoralisWebGL";
 
             Debug.Log("Done pre-processing webl template...");
         }
@@ -43,24 +42,24 @@ namespace MoralisWeb3ApiSdk.Editor
         /// <summary>
         /// Copies Moralis WebGLTemplate to required Assets/WebGLTemplate folder.
         /// </summary>
-        private void CopyWebGLTemplate()
-        {
-            var destinationFolder = Path.GetFullPath("Assets/WebGLTemplates/MoralisWebGL");
+        //private static void CopyWebGLTemplate()
+        //{
+        //    var destinationFolder = Path.GetFullPath("Assets/WebGLTemplates/MoralisWebGL");
 
-            var sourceFolder = Path.GetFullPath("Packages/io.moralis.metaversesdk/Resources/WebGLTemplates/MoralisWebGL");
+        //    var sourceFolder = Path.GetFullPath("Packages/io.moralis.metaversesdk/Resources/WebGLTemplates/MoralisWebGL");
 
-            Debug.Log($"Copying template from {sourceFolder}...");
+        //    Debug.Log($"Copying template from {sourceFolder}...");
 
-            FileUtil.ReplaceDirectory(sourceFolder, destinationFolder); 
+        //    FileUtil.ReplaceDirectory(sourceFolder, destinationFolder); 
             
-            AssetDatabase.Refresh();
+        //    AssetDatabase.Refresh();
 
-            Debug.Log($"Setting webgl template, old was = {PlayerSettings.WebGL.template}");
+        //    Debug.Log($"Setting webgl template, old was = {PlayerSettings.WebGL.template}");
 
-            PlayerSettings.WebGL.template = "PROJECT:MyTemplateName";
+        //    PlayerSettings.WebGL.template = "PROJECT:MyTemplateName";
 
-            Debug.Log($"Set webgl template to {PlayerSettings.WebGL.template}");
-        }
+        //    Debug.Log($"Set webgl template to {PlayerSettings.WebGL.template}");
+        //}
 
         /// <summary>
         /// Creates / Updates csc.rsp file for Moralis Updates in Asset Root .
@@ -68,76 +67,76 @@ namespace MoralisWeb3ApiSdk.Editor
         /// create a duplicate entry. If either becomes a necessity, add a process
         /// that removes entries before entires are appended.
         /// </summary>
-        private void UpdateCscFile()
-        {
-            // Expected Moralis entires
-            string[] moralisEntries = { "-define:MORALIS_UNITY" };
+        //private static void UpdateCscFile()
+        //{
+        //    // Expected Moralis entires
+        //    string[] moralisEntries = { "-define:MORALIS_UNITY" };
 
-            Debug.Log("Processing csc.rsp file for Moralis entries.");
+        //    Debug.Log("Processing csc.rsp file for Moralis entries.");
 
-            try
-            {
-                var destinationFile = Path.GetFullPath("Assets/csc.rsp");
+        //    try
+        //    {
+        //        var destinationFile = Path.GetFullPath("Assets/csc.rsp");
 
-                // If file exists, determin if it contains all moralis entries.
-                if (File.Exists(destinationFile))
-                {
-                    Debug.Log("csc.rsp exists, updating with Moralis entries ...");
+        //        // If file exists, determin if it contains all moralis entries.
+        //        if (File.Exists(destinationFile))
+        //        {
+        //            Debug.Log("csc.rsp exists, updating with Moralis entries ...");
 
-                    List<string> entries = new List<string>(moralisEntries);
+        //            List<string> entries = new List<string>(moralisEntries);
 
-                    // First look at the file for current moralis entries
-                    using (StreamReader sr = new StreamReader(destinationFile))
-                    {
-                        while (!sr.EndOfStream)
-                        {
-                            string e = sr.ReadLine();
+        //            // First look at the file for current moralis entries
+        //            using (StreamReader sr = new StreamReader(destinationFile))
+        //            {
+        //                while (!sr.EndOfStream)
+        //                {
+        //                    string e = sr.ReadLine();
 
-                            if (entries.Contains(e))
-                            {
-                                entries.Remove(e);
-                            }
-                        }
-                    }
+        //                    if (entries.Contains(e))
+        //                    {
+        //                        entries.Remove(e);
+        //                    }
+        //                }
+        //            }
 
-                    // If any moralis entries are not in the csc.rsp, append them.
-                    if (entries.Count > 0)
-                    {
-                        using (StreamWriter sw = new StreamWriter(destinationFile, true))
-                        {
-                            foreach (string e in entries)
-                            {
-                                sw.WriteLine(e);
-                            }
+        //            // If any moralis entries are not in the csc.rsp, append them.
+        //            if (entries.Count > 0)
+        //            {
+        //                using (StreamWriter sw = new StreamWriter(destinationFile, true))
+        //                {
+        //                    foreach (string e in entries)
+        //                    {
+        //                        sw.WriteLine(e);
+        //                    }
 
-                            sw.Flush();
-                        }
-                    }
-                }
-                // file does not exist so create it and populate the file with the moralis entries.
-                else
-                {
-                    Debug.Log("csc.rsp does not exist, creating ...");
+        //                    sw.Flush();
+        //                }
+        //            }
+        //        }
+        //        // file does not exist so create it and populate the file with the moralis entries.
+        //        else
+        //        {
+        //            Debug.Log("csc.rsp does not exist, creating ...");
 
-                    using (StreamWriter sw = new StreamWriter(destinationFile))
-                    {
-                        foreach (string e in moralisEntries)
-                        {
-                            sw.WriteLine(e);
-                        }
+        //            using (StreamWriter sw = new StreamWriter(destinationFile))
+        //            {
+        //                foreach (string e in moralisEntries)
+        //                {
+        //                    sw.WriteLine(e);
+        //                }
 
-                        sw.Flush();
-                    }
-                }
+        //                sw.Flush();
+        //            }
+        //        }
 
-                Debug.Log("csc.rsp files has been processed for Moralis entries.");
+        //        Debug.Log("csc.rsp files has been processed for Moralis entries.");
 
-                AssetDatabase.Refresh();
-            }
-            catch (Exception exp)
-            {
-                Debug.LogError($"Error encountered processing csc.rsp file for Moralis entries: {exp.Message}");
-            }
-        }
+        //        AssetDatabase.Refresh();
+        //    }
+        //    catch (Exception exp)
+        //    {
+        //        Debug.LogError($"Error encountered processing csc.rsp file for Moralis entries: {exp.Message}");
+        //    }
+        //}
     }
 }
